5cf0873c1dea9f3b0597282a170ff3edc6c9621b
Winterblade49 <Winterblade49@gmail.com>
2024-10-05 22:11:11 -0600

    Updated Calc Ui
    scaled buttons correcly and made basic Ui

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -11,10 +11,10 @@ class Calc_Buttons(tk.Frame):
         super().__init__(master)
         self.pack()  
         
-    def Create_button(self,Button_Name,):
+    def Create_button(self,Button_Name,row=0,colum=0,button_size=2):
         # Creates a button
-        self.button = tk.Button(self, text=Button_Name, command= lambda name=Button_Name: self.button_clicked(name) )
-        self.button.grid(row=1,column=1)
+        self.button = tk.Button(self, text=Button_Name, command= lambda name=Button_Name: self.button_clicked(name),font=("Helvetica", button_size),width=2)
+        self.button.grid(row=row,column=colum)
 
 
     
@@ -29,21 +29,37 @@ class App(tk.Frame):
         super().__init__(master)
         self.pack()
         
+        # Create an input field at the top
+        self.input_field = tk.Entry(self, width=10, font=("Helvetica", 24))
+        self.input_field.pack(side=tk.TOP)
+        
+        
         #def instance od Calc buttons class
         Buttons = Calc_Buttons(self)
 
         #creates all 9 number buttons
         for i in range(0,10,1):
-            Buttons.Create_button(i)
+            
+            row = (i - 1) // 3 + 1
+            colum = (i - 1) % 3
+            
+            if  i == 0:
+                row = 4
+                colum = 1
+
+            Buttons.Create_button(i,row,colum,button_size = 24)
             
         #creates all operand buttons   
-        Operands = ("x","y","-","+",)    
+        Operands = ("x","/","+","-",)    
+        row = 1
         for value in Operands:
-            i = 0
-            i+1
-            Buttons.Create_button(value)
+            Buttons.Create_button(value,row, 3, button_size = 24)
+            row += 1
         
         
+
+
+
 
 # create the application        
 myapp = App()
@@ -58,4 +74,6 @@ myapp.master.maxsize(Calc_Width, Calc_Height)
 myapp.master.iconbitmap('Calculator/Images/Calculator_Icon.ico')
 # start the program
 myapp.mainloop()
+
+
 
